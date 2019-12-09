1036bb18f282bc4ea79200d8b7ac4a92aa171abd
Muey Surigao <muey.surigao@gmail.com>
2019-12-08 16:49:13 -0800

    added all files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheeseburger');
+
+INSERT INTO burgers (burger_name) VALUES ('Truffle Waffle Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Blue Cheese Bison Burger'); 
